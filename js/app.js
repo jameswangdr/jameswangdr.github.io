6fdafa7d0c7e79516b4dcf365229421fcf6c6174
jameswang <jameswangdr@gmail.com>
2019-09-03 22:00:05 -0700

    fixed typo, restyle

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+        $(this).toggleClass('active');
+    });
+});
+
+lightbox.option({
+    'albumLabel': "© James Wang 2019"
+})
